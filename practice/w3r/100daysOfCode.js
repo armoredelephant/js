34205ab10ca89b1dd0ea8d23a7fdb33357aa5187
Keith Alleman <keith.alleman@icloud.com>
2018-09-02 09:28:54 -0600

    freeCodeCamp question

--- practice/w3r/100daysOfCode.js
+++ practice/w3r/100daysOfCode.js
@@ -8,7 +8,7 @@ function filteredArray(arr, elem) {
     let newArr = [];
     // loop through the indexes of the passed through array
     for (let i = 0; i < arr.length; i++) {
-        if (arr[i].indexOf(elem) != -1) {
+        if (arr[i].indexOf(elem) === -1) {
             newArr.push(arr[i]);
         }
     }
@@ -17,10 +17,8 @@ function filteredArray(arr, elem) {
 }
 
 console.log(filteredArray([[1,2,3],[4,5,6]],1));
-// => [1,2,3];
 
 console.log(filteredArray([[9,5,1],[16,3,67],[1,2,4],[9,5,1]], 1));
-// => [ [ 9, 5, 1 ], [ 1, 2, 4 ], [ 9, 5, 1 ] ]
 
 console.log(filteredArray([['word', 2], ['another', 1], ['final', 9]], 1));
 
