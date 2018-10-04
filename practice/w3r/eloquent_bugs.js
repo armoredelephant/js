adf840c91ce07f7c8f869b775368f154197ffc03
Keith Alleman <keith.alleman@icloud.com>
2018-10-03 19:27:17 -0600

    Further studying on bugs & errors. Started watching tutorials on using Chrome debugger with breakpoints

--- practice/w3r/eloquent_bugs.js
+++ practice/w3r/eloquent_bugs.js
@@ -31,4 +31,55 @@ let ferdinand = Person("Ferdinand"); // forgot new
 
 // putting "use strict" at the top of the program rarely hurts and might help spot problem
 
+/*************************************************************
+ * Types
+ */
 
+// Very useful to leave comments explaining what type the arguments are on func
+
+// (VillageState, Array) => {direction: string, memory: array}
+function goalOrientedRobot(state, memory) {}
+
+/*************************************************************
+ * Testing
+ */
+
+function test(label, body) {
+    if (!body()) console.log(`Failed: ${label}`); //if body() returns false, it will console.log faile
+}
+
+test("convert Latin text to uppercase", () => {
+    return "hello".toUpperCase() == "HELLO";
+}); // hello.toUpperCase is HELLO so nothing would get logged.
+
+// the more external objects that the code interacts with, the harder it is
+// to set up the context in which to test it
+
+/*************************************************************
+ * Debugging
+ */
+
+function numberToString(n, base = 10) { // base by default is set to 10
+    let result = "", sign = ""; // results and sign are both empty strings
+    if (n < 0) { // if n is less than zero, sign  is - and n = -n 
+      sign = "-";
+      n = -n;
+    }
+    do { // while n is greater than 0, we set result to 
+      result = String(n % base) + result; // String(n % base) is the remaineder
+      n /= base; //diving n by the base 
+    } while (n > 0);
+    return sign + result;
+  }
+  console.log(numberToString(13, 10));
+
+/**
+ * This is where you must resist the urge to start making random changes 
+ * to the code to see whether that makes it better. Instead, think. 
+ * Analyze what is happening and come up with a theory of why it might 
+ * be happening. Then, make additional observations to test this theory—or, 
+ * if you don’t yet have a theory, make additional observations to help you 
+ * come up with one.
+ */
+
+debugger
