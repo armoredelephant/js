7207cc4f8d897c6e417ff3b61f53535d5caa8b19
Keith Alleman <keith.alleman@icloud.com>
2018-10-30 17:38:59 -0600

    Another Kata. level 6 but was crazy easy.

--- practice/w3r/codewars.js
+++ practice/w3r/codewars.js
@@ -680,3 +680,34 @@ function domainName(url){
  // ******************************************************************
 // *** NEXT ***
 // ******************************************************************
+
+// Array.diff
+
+/**
+ * Your goal in this kata is to implement a difference function.
+ * It will subtract one list from another and returns the result
+ * 
+ * It should remove all values from list a, which are present in list b
+ * 
+ * array_diff([1,2], [1]) == [2]
+ * 
+ * If a value is present in b, all of it's occurrences must be removed from the other:
+ * 
+ * array_diff([1,2,2,2,3], [2]) = [1,3]
+ */
+
+ function array_diff(array1, array2) {
+     // filter array a where item is not also found in array2
+    return array1.filter( i => array2.indexOf(i) == -1);
+}
+
+// *** includes ***
+
+function array_diff(array1, array2) {
+    return array1.filter(i => !b.includes(i));
+}
+
+// ******************************************************************
+// *** NEXT ***
+// ******************************************************************
+
